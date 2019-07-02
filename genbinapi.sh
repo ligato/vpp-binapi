b10c55688a8b85f3f1b53eb114c29fa954364cf6
Ondrej Fabry <ofabry@cisco.com>
2019-07-02 12:21:33 +0200

    Update genbinapi.sh script and .travis.yml
    Signed-off-by: Ondrej Fabry <ofabry@cisco.com>

--- genbinapi.sh
+++ genbinapi.sh
@@ -10,6 +10,8 @@ go install -v git.fd.io/govpp.git/cmd/binapi-generator
 
 docker run --rm "$VPP_IMG" cat /vpp/version > /tmp/VPP_VERSION
 
+echo "Generating binapi for VPP $(cat /tmp/VPP_VERSION)"
+
 rm -rf vppapi/*
 docker run --rm -v $(pwd)/vppapi:/vppapi -w /vppapi -u $(id -u):$(id -g) "$VPP_IMG" \
 	sh -c "cp -r /usr/share/vpp/api/* /vppapi"
